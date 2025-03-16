b9425abe9cafc2190008be627b46e67bbd0cbcfb
ericdourado <ericdourado1@hotmail.com>
2025-03-16 17:40:48 -0300

    rota de criar filmes criada com validacoes

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,24 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : Controller
+{
+    private List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void adicionaFilme([FromBody] Filme filme)
+    {
+
+
+        filmes.Add(filme);
+
+
+
+
+    }
+
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage ="O Título é obrigatório")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O Genero é obrigatório")]
+    public string Genero { get; set; }
+    [Required(ErrorMessage = "A Duração é obrigatória")]
+    [Range(70,600, ErrorMessage ="A Duração deve ser entre 70 a 600 segundos para ser considerado um filme")]
+    public string Duracao { get; set; }
+
+}
